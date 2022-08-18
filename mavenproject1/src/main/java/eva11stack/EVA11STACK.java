7cbaffa6341d32137cd446738535210f793ce422
BryanQuintanaTV <78245905+BryanQuintanaTV@users.noreply.github.com>
2022-08-18 09:47:16 -0600

    EVA1-1-STACK
    Practica 1
    18/08/2022

---
+++ mavenproject1/src/main/java/eva11stack/EVA11STACK.java
@@ -0,0 +1,25 @@
+package eva11stack;
+/*
+ * "Practica 1"
+ * "EVA1-1-Stack"
+ */
+
+/**
+ * @author Quintana Juarez Bryan Alexis
+ */
+public class EVA11STACK {
+    
+    public static void main(String[] args) {
+        A();
+    }
+    
+    public static void A(){
+        B();
+    }
+    
+    public static void B(){
+        int i = 1;
+        double d = 10.0;
+    }
+    
+}
